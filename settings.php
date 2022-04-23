665036a214ede25b2485d6bd27f4a2d282f31525
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2022-04-23 19:11:32 +1000

    addedd support for authkey header

--- settings.php
+++ settings.php
@@ -1,6 +1,16 @@
 <?php
 $address = "http://your-domain.com:4567";
 
+// Create a stream
+$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "key: you-password\r\n",
+    ]
+];
+// DOCS: https://www.php.net/manual/en/function.stream-context-create.php
+$context = stream_context_create($opts);
+
 //query http://your-domain.com:4567/v1/worlds/ for world IDs
 $overworld = "world_ID";
 $nether = "nether_ID";
