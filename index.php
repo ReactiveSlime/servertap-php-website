2a4e072e86482da1026107e839d54283ce4d40a8
ReactiveSlime <33995597+ReactiveSlime@users.noreply.github.com>
2022-04-29 07:40:28 +1000

    moved some stuff to settings.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beta Playerstats page</title>
-    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!---<script type='text/javascript' src='file.js'></script>--->
 </head>
 
 <body>
@@ -14,35 +15,19 @@
     require_once('func.php');
     require_once('settings.php');
 
-    //echo "<script type='text/javascript' src='file.js'></script>";
-    //get json data from url
-
-
-    $players_url= $address . '/v1/players/';
-    $server_stats_url = $address . '/v1/server/';
-    $world_stats_url = $address . '/v1/worlds/' . $overworld;
-
-    $players = file_get_contents( $players_url, false,  $context,);
-    $server_stats = file_get_contents( $server_stats_url, false, $context,);
-    $world_stats = file_get_contents( $world_stats_url, false, $context,);
-    $world_stats = json_decode($world_stats);
-    $server = json_decode($server_stats);
-
-
-
-
     /* #region server-stats */
     //SERVER STATS
+
+    //Convert server ticks to 24 hour time. 0 ticks being 6am and 5:59:59am is 24000 ticks.
+    $server_ticks = $world_stats->time;
 
     echo "The server TPS is " . $server->tps . "<br>";
 
     //prints uptime
     $seconds = $server->health->uptime;
-    echo "The server Uptime is " . secondsToTime($seconds) . "<br>";
+    echo "The server uptime is " . secondsToTime($seconds) . "<br>";
 
     //check to see if it is storming . if it is also check to see if it is thundering else its clear
-    //world_stats>storm true or false
-    //world_stats>thundering true or false
     if ($world_stats->storm == true) {
         if ($world_stats->thundering == true) {
             echo "The server weather thundering<br>";
@@ -52,6 +37,7 @@
     } else {
         echo "The server weather is clear<br>";
     }
+    
     /* #endregion */
     //PLAYER STATS
     //check how many players are online and print the number
